8e4a6db808eab2edbe857767fb193ed648fb2917
Gregor Graham <gregorsgraham@gmail.com>
2026-05-17 20:46:06 -0700

    Deploy 2026-05-17 20:46

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // CACHE_VERSION whenever index.html or manifest.json change so the new
 // shell is fetched instead of served from the old cache.
 
-const CACHE_VERSION = 'jobquoter-v10';
+const CACHE_VERSION = 'jobquoter-v11';
 const SHELL = [
   './',
   './index.html',
